1af41fb5fb38d2914eaee2c91338f38ba79a1f7a
GenievreThelonelyHunter <126477952+GenievreThelonelyHunter@users.noreply.github.com>
2024-05-23 11:33:45 +0200

    Visualization

--- progetto dma.R
+++ progetto dma.R
@@ -4,16 +4,34 @@ library(tidyverse)
 
 netflix <- read.csv("C:/Users/Alessandro Caminiti/Desktop/per il progetto di dma/Netflix.csv")
 names(netflix)
-scores <- netflix%>%drop_na()%>%select(title,imdb_score,tmdb_score,tmdb_popularity)
-ggplot=(sequence) + geom_point()
-scores
+scores <- netflix%>%select(title,imdb_score,tmdb_score,tmdb_popularity)
+
+ggplot(data = scores, mapping = aes(x=imdb_score,y =tmdb_score)) +
+  geom_point()
+
+
+ggplot(data = scores, mapping = aes(x=imdb_score)) +
+  geom_histogram()
+
+ggplot(data = scores, mapping = aes(x=tmdb_score)) +
+  geom_histogram()
+
+scores <- scores%>%drop_na()
+
+ggplot(data = scores, mapping = aes(x=tmdb_score)) +
+  geom_histogram()
+
+
 ggplot(data = scores, mapping = aes(x=imdb_score)) +
 geom_histogram()
+
+
 ggsave("scatterplot.png", width = 9, height = 7, dpi = 1000)
 ggplot(data = scores, mapping = aes(x=tmdb_score)) +
   geom_histogram()
+
 ggplot(data = scores, mapping = aes(x=imdb_score,y =tmdb_score)) +
 geom_point()
 
-??tidyverse
+
 
