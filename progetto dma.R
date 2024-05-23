f9196a6a910aa4ec30f26fdb6b66ee203f909bdb
Alessandro Maria Caminiti <alessandrocaminiti039@gmail.com>
2024-05-23 11:03:26 +0200

    First commit

---
+++ progetto dma.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(tidyverse)
+
+
+netflix <- read.csv("C:/Users/Alessandro Caminiti/Desktop/per il progetto di dma/Netflix.csv")
+names(netflix)
+scores <- netflix%>%drop_na()%>%select(title,imdb_score,tmdb_score,tmdb_popularity)
+ggplot=(sequence) + geom_point()
+scores
+ggplot(data = scores, mapping = aes(x=imdb_score)) +
+geom_histogram()
+ggsave("scatterplot.png", width = 9, height = 7, dpi = 1000)
+ggplot(data = scores, mapping = aes(x=tmdb_score)) +
+  geom_histogram()
+ggplot(data = scores, mapping = aes(x=imdb_score,y =tmdb_score)) +
+geom_point()
+
+??tidyverse
+
